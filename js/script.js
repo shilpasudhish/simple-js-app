f33f7c3ef6efb8e7e4651c07de2a95d0c9e11896
shilpasudhish <bindultali@gmail.com>
2024-10-08 00:58:16 +0200

    Update script.js
    Added pokemonList array, with 3 objects.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-favoriteFood = 'Tea';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass','poison']
+    },
+    {
+        name: 'Butterfree',
+        height: 1.1,
+        types: ['Bug']
+    },
+    {
+        name: 'Caterpie',
+        height: 0.3,
+        types: ['Bug','Shield-dust']
+    }
+];
