06deb42463b52a326e27475724b12add6efefee8
shilpasudhish <bindultali@gmail.com>
2024-10-09 00:29:13 +0200

    Updated script.js with for loop and if condition.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+//Pokemon Objects
+
 let pokemonList = [
     {
         name: 'Bulbasaur',
-        height: 7,
+        height: 1.7,
         types: ['grass','poison']
     },
     {
@@ -15,3 +17,18 @@ let pokemonList = [
         types: ['Bug','Shield-dust']
     }
 ];
+
+//displays the name and height of the pokemon, and adds comment for the biggest pokemon.
+
+for (let i=0;i<pokemonList.length;i++)
+{
+    if (pokemonList[i].height>1.5)
+    {
+        document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height+ ') - Wow, that’s big!</p>');
+
+    }
+    else
+    {
+        document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height+ ')</p>');
+    }
+}
