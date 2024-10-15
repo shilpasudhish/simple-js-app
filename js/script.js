549aa42589fb565770a50e42d54925958cbab8d0
shilpasudhish <bindultali@gmail.com>
2024-10-15 17:38:40 +0200

    Updated js file with forEach

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 //Pokemon Objects
 
 let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 1.7,
-        types: ['grass','poison']
-    },
-    {
-        name: 'Butterfree',
-        height: 1.1,
-        types: ['Bug']
-    },
-    {
-        name: 'Caterpie',
-        height: 0.3,
-        types: ['Bug','Shield-dust']
-    }
+  {
+    name: "Bulbasaur",
+    height: 1.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Butterfree",
+    height: 1.1,
+    types: ["Bug"],
+  },
+  {
+    name: "Caterpie",
+    height: 0.3,
+    types: ["Bug", "Shield-dust"],
+  },
 ];
 
 //displays the name and height of the pokemon, and adds comment for the biggest pokemon.
-
-for (let i=0;i<pokemonList.length;i++)
-{
-    if (pokemonList[i].height>1.5)
-    {
-        document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height+ ') - Wow, that’s big!</p>');
-
-    }
-    else
-    {
-        document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height+ ')</p>');
-    }
-}
+pokemonList.forEach(function (pokemon) {
+  pokemon.height > 1.5
+    ? document.write(
+        "<p>" +
+          pokemon.name +
+          " (height:" +
+          pokemon.height +
+          ") - Wow, that’s big!</p>"
+      )
+    : document.write(
+        "<p>" + pokemon.name + " (height:" + pokemon.height + ")</p>"
+      );
+});
