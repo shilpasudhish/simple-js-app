8b18e107c09d95d00a8a184c6062d1882c0e1080
shilpasudhish <bindultali@gmail.com>
2024-10-15 20:11:17 +0200

    Update js file with forEach and IIFE

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 //Pokemon Objects
 
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 1.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Butterfree",
-    height: 1.1,
-    types: ["Bug"],
-  },
-  {
-    name: "Caterpie",
-    height: 0.3,
-    types: ["Bug", "Shield-dust"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 1.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Butterfree",
+      height: 1.1,
+      types: ["Bug"],
+    },
+    {
+      name: "Caterpie",
+      height: 0.3,
+      types: ["Bug", "Shield-dust"],
+    },
+  ];
+  function add(item) {
+    pokemonList.push(item);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
-//displays the name and height of the pokemon, and adds comment for the biggest pokemon.
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach((pokemon) => {
   pokemon.height > 1.5
     ? document.write(
         "<p>" +
